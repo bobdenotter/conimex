a0a17fd09d9a90a5982fa2e0373dcb158d7fb517
Bob den Otter <bob@twokings.nl>
2020-02-16 16:11:08 +0100

    Tweak, tweak

--- src/Extension.php
+++ src/Extension.php
@@ -14,45 +14,6 @@ class Extension extends BaseExtension
      */
     public function getName(): string
     {
-        return 'Conimex - Content, Import, Export';
-    }
-
-    /**
-     * Add the routes for this extension.
-     *
-     * Note: These are cached by Symfony. If you make modifications to this, run
-     * `bin/console cache:clear` to ensure your routes are parsed.
-     */
-//    public function getRoutes(): array
-//    {
-//        return [
-//            'reference' => new Route(
-//                '/extensions/reference/{name}',
-//                ['_controller' => 'BobdenOtter\Conimex\Controller::index'],
-//                ['name' => '[a-zA-Z0-9]+']
-//            ),
-//        ];
-//    }
-
-    /**
-     * Ran automatically, if the current request is in a browser.
-     * You can use this method to set up things in your extension.
-     *
-     * Note: This runs on every request. Make sure what happens here is quick
-     * and efficient.
-     */
-    public function initialize($cli = false): void
-    {
-    }
-
-    /**
-     * Ran automatically, if the current request is from the command line (CLI).
-     * You can use this method to set up things in your extension.
-     *
-     * Note: This runs on every request. Make sure what happens here is quick
-     * and efficient.
-     */
-    public function initializeCli(): void
-    {
+        return 'Conimex - Content Import & Export';
     }
 }
